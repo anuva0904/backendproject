c2713d3c93f74039f8273f1e65c61174d4f90a2f
Anuva Singh <singhanuva4@gmail.com>
2025-09-28 12:44:10 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const fs =require('fs');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+
+
+app.get('/',(req,res)=>{
+    fs.readdir(`./files`, (err,files)=>{
+        res.render('index',{ files : files });
+    });
+});
+
+
+
+app.listen(3000)
