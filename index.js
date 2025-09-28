7c152f4582c0f2399fe7e86d017f75e9e54ea2be
Anuva Singh <singhanuva4@gmail.com>
2025-09-28 13:33:30 +0530

    reading through the files data

--- index.js
+++ index.js
@@ -11,10 +11,18 @@ app.set('view engine', 'ejs');
 
 app.get('/',(req,res)=>{
     fs.readdir(`./files`, (err,files)=>{
+        // console.log(files);
         res.render('index',{ files : files });
+    });
+});
+
+app.post('/create',(req,res)=>{
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, (err)=>{
+        if(err) throw err;
+        res.redirect('/');
     });
 });
 
 
 
-app.listen(3000)
+app.listen(3000);
